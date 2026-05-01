625f177d340aee8c9a7fd5b7b11bf71c72fbf5c3
Iqbal <iqbalhossain6615@gmail.com>
2026-05-01 14:30:49 +0600

    added All Animals page

---
+++ src/app/all-animals/page.jsx
@@ -0,0 +1,20 @@
+import AnimalCard from '@/components/AnimalCard';
+import React from 'react';
+
+const AllAnimalsPage = async () => {
+    const res = await fetch('https://livestock-booking-platform-iqbal.vercel.app/data.json');
+    const animals = await res.json();
+    console.log(animals);
+    
+    return (
+        <div>
+            <h1 className='text-2xl font-bold m-4'>All Animals</h1>
+
+            <div className='grid grid-cols-4 gap-5'>
+                {animals.map(animal => <AnimalCard key={animal.id} animal={animal} />)}
+            </div>
+        </div>
+    );
+};
+
+export default AllAnimalsPage;
